2b02aa2f6cc37f37cb0d3b5d5ecd1bb7935f3cad
TOMAS ARIEL GOLDENTUL <49319223@ort>
2025-07-02 08:00:04 -0300

    Primer commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using tp_sinnumero.Models;
+
+namespace tp_sinnumero.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tp_sinnumero.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
